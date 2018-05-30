e35927cdcaccde85f9d1fa40361ac714776ff86f
vosferatu <up201505439@fe.up.pt>
2018-05-30 11:00:42 +0100

    change

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -247,6 +247,13 @@ class ContentController extends Controller
   public function save(Content $content)
   {
 
+    $var = $content->saves->where('id_user', Auth::user()->id)->first();
+    
+    if($var){
+      $var->delete();
+      return;
+    }
+
     Save::create([
       'id_content' => $content->id,
       'id_user' => Auth::user()->id,
@@ -266,13 +273,15 @@ class ContentController extends Controller
     if($downvote){
       $this->upvote($content);
       $downvote->delete();
-    } else if(!$upvote){
+    } else {
+            if(!$downvote && !$upvote){
               $this->upvote($content);
-          } else {
-            $upvote->delete;
+          } else if ($upvote && !$downvote) {
+            $upvote->delete();
           }
+    }
 
-    $num = $content->votes;
+    $num = Content::find($content->id)->votes;
 
     return response()->json(['votes'=>$num]);
   }
@@ -288,13 +297,15 @@ class ContentController extends Controller
     if($upvote){
       $this->downvote($content);
       $upvote->delete();
-    } else if(!$downvote){
+    } else {
+        if(!$downvote && !$upvote){
               $this->downvote($content);
-          } else {
-              $downvote->delete;
+          } else if ($downvote && !$upvote) {
+              $downvote->delete();
           }
-
-    $num = $content->votes;
+      }
+      
+    $num = Content::find($content->id)->votes;
 
     return response()->json(['votes'=>$num]);
   }
